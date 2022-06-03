61921f0c2cf10f014d24832e738ffa72888cfda1
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2022-06-03 22:06:01 +0600

    Modify configs only if true

--- nginx/docker-entrypoint.d/50-common-config.sh
+++ nginx/docker-entrypoint.d/50-common-config.sh
@@ -1,71 +1,74 @@
 #!/bin/bash
 set -e;
 
-## CONF
+if [ "${NGINX_MODIFY_CONFIGS}" = "true" ]; then
 
-sed -i "s!\${NGINX_CONF_USER}!${NGINX_CONF_USER}!" /etc/nginx/nginx.conf
-sed -i "s!\${NGINX_CONF_WORKER_PROCESSES}!${NGINX_CONF_WORKER_PROCESSES}!" /etc/nginx/nginx.conf
-sed -i "s!\${NGINX_CONF_EVENTS_WORKER_CONNECTIONS}!${NGINX_CONF_EVENTS_WORKER_CONNECTIONS}!" /etc/nginx/nginx.conf
-sed -i "s!\${NGINX_CONF_HTTP_KEEPALIVE_TIMEOUT}!${NGINX_CONF_HTTP_KEEPALIVE_TIMEOUT}!" /etc/nginx/nginx.conf
-sed -i "s!\${NGINX_CONF_HTTP_GZIP_STATIC}!${NGINX_CONF_HTTP_GZIP_STATIC}!" /etc/nginx/nginx.conf
-sed -i "s!\${NGINX_CONF_HTTP_GZIP_COMP_LEVEL}!${NGINX_CONF_HTTP_GZIP_COMP_LEVEL}!" /etc/nginx/nginx.conf
-sed -i "s!\${NGINX_CONF_HTTP_CLIENT_MAX_BODY_SIZE}!${NGINX_CONF_HTTP_CLIENT_MAX_BODY_SIZE}!" /etc/nginx/nginx.conf
+    ## CONF
+    sed -i "s!\${NGINX_CONF_USER}!${NGINX_CONF_USER}!" /etc/nginx/nginx.conf
+    sed -i "s!\${NGINX_CONF_WORKER_PROCESSES}!${NGINX_CONF_WORKER_PROCESSES}!" /etc/nginx/nginx.conf
+    sed -i "s!\${NGINX_CONF_EVENTS_WORKER_CONNECTIONS}!${NGINX_CONF_EVENTS_WORKER_CONNECTIONS}!" /etc/nginx/nginx.conf
+    sed -i "s!\${NGINX_CONF_HTTP_KEEPALIVE_TIMEOUT}!${NGINX_CONF_HTTP_KEEPALIVE_TIMEOUT}!" /etc/nginx/nginx.conf
+    sed -i "s!\${NGINX_CONF_HTTP_GZIP_STATIC}!${NGINX_CONF_HTTP_GZIP_STATIC}!" /etc/nginx/nginx.conf
+    sed -i "s!\${NGINX_CONF_HTTP_GZIP_COMP_LEVEL}!${NGINX_CONF_HTTP_GZIP_COMP_LEVEL}!" /etc/nginx/nginx.conf
+    sed -i "s!\${NGINX_CONF_HTTP_CLIENT_MAX_BODY_SIZE}!${NGINX_CONF_HTTP_CLIENT_MAX_BODY_SIZE}!" /etc/nginx/nginx.conf
 
-## VHOST
+    ## VHOST
+    sed -i "s!\${NGINX_VHOST_HTTP_LISTEN_PORT}!${NGINX_VHOST_HTTP_LISTEN_PORT}!" /etc/nginx/conf.d/*
+    sed -i "s!\${NGINX_VHOST_HTTPS_LISTEN_PORT}!${NGINX_VHOST_HTTPS_LISTEN_PORT}!" /etc/nginx/conf.d/*
+    sed -i "s!\${NGINX_VHOST_HTTP_SERVER_NAME}!${NGINX_VHOST_HTTP_SERVER_NAME}!" /etc/nginx/conf.d/*
+    sed -i "s!\${NGINX_VHOST_HTTPS_SERVER_NAME}!${NGINX_VHOST_HTTPS_SERVER_NAME}!" /etc/nginx/conf.d/*
+    sed -i "s!\${NGINX_VHOST_DOCUMENT_ROOT}!${NGINX_VHOST_DOCUMENT_ROOT}!" /etc/nginx/conf.d/*
+    sed -i "s!\${NGINX_VHOST_REDIRECT_FROM_SERVER_NAME}!${NGINX_VHOST_REDIRECT_FROM_SERVER_NAME}!" /etc/nginx/conf.d/*
+    sed -i "s!\${NGINX_VHOST_REDIRECT_TO_PROTOCOL}!${NGINX_VHOST_REDIRECT_TO_PROTOCOL}!" /etc/nginx/conf.d/*
+    sed -i "s!\${NGINX_VHOST_REDIRECT_TO_SERVER_NAME}!${NGINX_VHOST_REDIRECT_TO_SERVER_NAME}!" /etc/nginx/conf.d/*
 
-sed -i "s!\${NGINX_VHOST_HTTP_LISTEN_PORT}!${NGINX_VHOST_HTTP_LISTEN_PORT}!" /etc/nginx/conf.d/*
-sed -i "s!\${NGINX_VHOST_HTTPS_LISTEN_PORT}!${NGINX_VHOST_HTTPS_LISTEN_PORT}!" /etc/nginx/conf.d/*
-sed -i "s!\${NGINX_VHOST_HTTP_SERVER_NAME}!${NGINX_VHOST_HTTP_SERVER_NAME}!" /etc/nginx/conf.d/*
-sed -i "s!\${NGINX_VHOST_HTTPS_SERVER_NAME}!${NGINX_VHOST_HTTPS_SERVER_NAME}!" /etc/nginx/conf.d/*
-sed -i "s!\${NGINX_VHOST_DOCUMENT_ROOT}!${NGINX_VHOST_DOCUMENT_ROOT}!" /etc/nginx/conf.d/*
-sed -i "s!\${NGINX_VHOST_REDIRECT_FROM_SERVER_NAME}!${NGINX_VHOST_REDIRECT_FROM_SERVER_NAME}!" /etc/nginx/conf.d/*
-sed -i "s!\${NGINX_VHOST_REDIRECT_TO_PROTOCOL}!${NGINX_VHOST_REDIRECT_TO_PROTOCOL}!" /etc/nginx/conf.d/*
-sed -i "s!\${NGINX_VHOST_REDIRECT_TO_SERVER_NAME}!${NGINX_VHOST_REDIRECT_TO_SERVER_NAME}!" /etc/nginx/conf.d/*
+    sed -i "s!\${NGINX_VHOST_SSL_CERTIFICATE}!${NGINX_VHOST_SSL_CERTIFICATE}!" /etc/nginx/includes/ssl-params.conf
+    sed -i "s!\${NGINX_VHOST_SSL_CERTIFICATE_KEY}!${NGINX_VHOST_SSL_CERTIFICATE_KEY}!" /etc/nginx/includes/ssl-params.conf
 
-sed -i "s!\${NGINX_VHOST_SSL_CERTIFICATE}!${NGINX_VHOST_SSL_CERTIFICATE}!" /etc/nginx/includes/ssl-params.conf
-sed -i "s!\${NGINX_VHOST_SSL_CERTIFICATE_KEY}!${NGINX_VHOST_SSL_CERTIFICATE_KEY}!" /etc/nginx/includes/ssl-params.conf
+    sed -i "s!\${NGINX_VHOST_DNS_RESOLVER_IP}!${NGINX_VHOST_DNS_RESOLVER_IP}!" /etc/nginx/includes/loc-*.conf
 
-sed -i "s!\${NGINX_VHOST_DNS_RESOLVER_IP}!${NGINX_VHOST_DNS_RESOLVER_IP}!" /etc/nginx/includes/loc-*.conf
+    sed -i "s!\${NGINX_VHOST_LOCATION_ROOT_TRY_FILES}!${NGINX_VHOST_LOCATION_ROOT_TRY_FILES}!" /etc/nginx/includes/loc-*.conf
 
-sed -i "s!\${NGINX_VHOST_LOCATION_ROOT_TRY_FILES}!${NGINX_VHOST_LOCATION_ROOT_TRY_FILES}!" /etc/nginx/includes/loc-*.conf
+    sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_PHPFPM_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-phpfpm.conf
+    sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_READ_TIMEOUT}!${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_READ_TIMEOUT}!" /etc/nginx/includes/loc-phpfpm.conf
+    sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_PASS}!${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_PASS}!" /etc/nginx/includes/loc-phpfpm.conf
 
-sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_PHPFPM_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-phpfpm.conf
-sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_READ_TIMEOUT}!${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_READ_TIMEOUT}!" /etc/nginx/includes/loc-phpfpm.conf
-sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_PASS}!${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_PASS}!" /etc/nginx/includes/loc-phpfpm.conf
+    sed -i "s!\${NGINX_VHOST_UPSTREAM_ECHO_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_ECHO_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-echo.conf
+    sed -i "s!\${NGINX_VHOST_UPSTREAM_MINIO_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_MINIO_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-minio.conf
 
-sed -i "s!\${NGINX_VHOST_UPSTREAM_ECHO_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_ECHO_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-echo.conf
-sed -i "s!\${NGINX_VHOST_UPSTREAM_MINIO_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_MINIO_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-minio.conf
-
-if [ "${NGINX_VHOST_ENABLE_HTTP_TRAFFIC}" = "false" ] && [ -f "/etc/nginx/conf.d/default.conf" ]; then
-    rm -f /etc/nginx/conf.d/default.conf
-fi
-
-if [ "${NGINX_VHOST_ENABLE_HTTPS_TRAFFIC}" = "false" ] && [ -f "/etc/nginx/conf.d/default-ssl.conf" ]; then
-    rm -f /etc/nginx/conf.d/default-ssl.conf
-fi
-
-if [ "${NGINX_VHOST_ENABLE_REDIRECT_HTTP_TO_HTTPS}" = "false" ] && [ -f "/etc/nginx/conf.d/redirect-http-https.conf" ]; then
-    rm -f /etc/nginx/conf.d/redirect-http-https.conf
-fi
-
-if [ "${NGINX_VHOST_ENABLE_REDIRECT_FROM_TO}" = "false" ]; then
-    if [ "${NGINX_VHOST_ENABLE_HTTP_TRAFFIC}" = "false" ] && [ -f "/etc/nginx/conf.d/redirect-from-to-http.conf" ]; then
-        rm -f /etc/nginx/conf.d/redirect-from-to-http.conf
+    # Files removal based on condition
+    if [ "${NGINX_VHOST_ENABLE_HTTP_TRAFFIC}" = "false" ] && [ -f "/etc/nginx/conf.d/default.conf" ]; then
+        rm -f /etc/nginx/conf.d/default.conf
     fi
 
-    if [ "${NGINX_VHOST_ENABLE_HTTPS_TRAFFIC}" = "false" ] && [ -f "/etc/nginx/conf.d/redirect-from-to-https.conf" ]; then
-        rm -f /etc/nginx/conf.d/redirect-from-to-https.conf
+    if [ "${NGINX_VHOST_ENABLE_HTTPS_TRAFFIC}" = "false" ] && [ -f "/etc/nginx/conf.d/default-ssl.conf" ]; then
+        rm -f /etc/nginx/conf.d/default-ssl.conf
     fi
-fi
 
-if [ "${NGINX_VHOST_USE_PHPFPM}" = "false" ] && [ -f "/etc/nginx/includes/loc-phpfpm.conf" ]; then
-    rm -f /etc/nginx/includes/loc-phpfpm.conf
-fi
+    if [ "${NGINX_VHOST_ENABLE_REDIRECT_HTTP_TO_HTTPS}" = "false" ] && [ -f "/etc/nginx/conf.d/redirect-http-https.conf" ]; then
+        rm -f /etc/nginx/conf.d/redirect-http-https.conf
+    fi
 
-if [ "${NGINX_VHOST_USE_ECHO}" = "false" ] && [ -f "/etc/nginx/includes/loc-echo.conf" ]; then
-    rm -f /etc/nginx/includes/loc-echo.conf
-fi
+    if [ "${NGINX_VHOST_ENABLE_REDIRECT_FROM_TO}" = "false" ]; then
+        if [ "${NGINX_VHOST_ENABLE_HTTP_TRAFFIC}" = "false" ] && [ -f "/etc/nginx/conf.d/redirect-from-to-http.conf" ]; then
+            rm -f /etc/nginx/conf.d/redirect-from-to-http.conf
+        fi
 
-if [ "${NGINX_VHOST_USE_MINIO}" = "false" ] && [ -f "/etc/nginx/includes/loc-minio.conf"]; then
-    rm -f /etc/nginx/includes/loc-minio.conf
+        if [ "${NGINX_VHOST_ENABLE_HTTPS_TRAFFIC}" = "false" ] && [ -f "/etc/nginx/conf.d/redirect-from-to-https.conf" ]; then
+            rm -f /etc/nginx/conf.d/redirect-from-to-https.conf
+        fi
+    fi
+
+    if [ "${NGINX_VHOST_USE_PHPFPM}" = "false" ] && [ -f "/etc/nginx/includes/loc-phpfpm.conf" ]; then
+        rm -f /etc/nginx/includes/loc-phpfpm.conf
+    fi
+
+    if [ "${NGINX_VHOST_USE_ECHO}" = "false" ] && [ -f "/etc/nginx/includes/loc-echo.conf" ]; then
+        rm -f /etc/nginx/includes/loc-echo.conf
+    fi
+
+    if [ "${NGINX_VHOST_USE_MINIO}" = "false" ] && [ -f "/etc/nginx/includes/loc-minio.conf"]; then
+        rm -f /etc/nginx/includes/loc-minio.conf
+    fi
+
 fi
