1dfa520df92caece50c109e4654b3404d8e7bc0c
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2022-06-03 23:25:20 +0600

    Fix file not found issue on restart
    If container is not fully down then during retry, it fails to find files
    because it tries to run from the exited container where files may have
    been deleted. So check if file exists before modifying or removal.

--- nginx/docker-entrypoint.d/50-common-config.sh
+++ nginx/docker-entrypoint.d/50-common-config.sh
@@ -3,7 +3,10 @@ set -e;
 
 if [ "${NGINX_MODIFY_CONFIGS}" = "true" ]; then
 
+    echo '!!! NGINX MODIFYING CONFIGS based on ENV !!!'
+
     ## CONF
+    echo '--- Updating /etc/nginx/nginx.conf ---'
     sed -i "s!\${NGINX_CONF_USER}!${NGINX_CONF_USER}!" /etc/nginx/nginx.conf
     sed -i "s!\${NGINX_CONF_WORKER_PROCESSES}!${NGINX_CONF_WORKER_PROCESSES}!" /etc/nginx/nginx.conf
     sed -i "s!\${NGINX_CONF_EVENTS_WORKER_CONNECTIONS}!${NGINX_CONF_EVENTS_WORKER_CONNECTIONS}!" /etc/nginx/nginx.conf
@@ -15,6 +18,7 @@ if [ "${NGINX_MODIFY_CONFIGS}" = "true" ]; then
     sed -i "s!\${NGINX_CONF_HTTP_CLIENT_MAX_BODY_SIZE}!${NGINX_CONF_HTTP_CLIENT_MAX_BODY_SIZE}!" /etc/nginx/nginx.conf
 
     ## VHOST
+    echo '--- Updating /etc/nginx/conf.d/* ---'
     sed -i "s!\${NGINX_VHOST_HTTP_LISTEN_PORT}!${NGINX_VHOST_HTTP_LISTEN_PORT}!" /etc/nginx/conf.d/*
     sed -i "s!\${NGINX_VHOST_HTTPS_LISTEN_PORT}!${NGINX_VHOST_HTTPS_LISTEN_PORT}!" /etc/nginx/conf.d/*
     sed -i "s!\${NGINX_VHOST_HTTP_SERVER_NAME}!${NGINX_VHOST_HTTP_SERVER_NAME}!" /etc/nginx/conf.d/*
@@ -24,29 +28,52 @@ if [ "${NGINX_MODIFY_CONFIGS}" = "true" ]; then
     sed -i "s!\${NGINX_VHOST_REDIRECT_TO_PROTOCOL}!${NGINX_VHOST_REDIRECT_TO_PROTOCOL}!" /etc/nginx/conf.d/*
     sed -i "s!\${NGINX_VHOST_REDIRECT_TO_SERVER_NAME}!${NGINX_VHOST_REDIRECT_TO_SERVER_NAME}!" /etc/nginx/conf.d/*
 
+    echo '--- Updating /etc/nginx/includes/ssl-params.conf ---'
     sed -i "s!\${NGINX_VHOST_SSL_CERTIFICATE}!${NGINX_VHOST_SSL_CERTIFICATE}!" /etc/nginx/includes/ssl-params.conf
     sed -i "s!\${NGINX_VHOST_SSL_CERTIFICATE_KEY}!${NGINX_VHOST_SSL_CERTIFICATE_KEY}!" /etc/nginx/includes/ssl-params.conf
 
+    echo '--- Updating /etc/nginx/includes/loc-*.conf ---'
+    sed -i "s!\${NGINX_VHOST_LOCATION_ROOT_TRY_FILES}!${NGINX_VHOST_LOCATION_ROOT_TRY_FILES}!" /etc/nginx/includes/loc-root.conf
+
+    echo '--- Updating /etc/nginx/includes/loc-*.conf for DNS Resolver ---'
     sed -i "s!\${NGINX_VHOST_DNS_RESOLVER_IP}!${NGINX_VHOST_DNS_RESOLVER_IP}!" /etc/nginx/includes/loc-*.conf
 
-    sed -i "s!\${NGINX_VHOST_LOCATION_ROOT_TRY_FILES}!${NGINX_VHOST_LOCATION_ROOT_TRY_FILES}!" /etc/nginx/includes/loc-*.conf
+    if [ -f "/etc/nginx/includes/loc-phpfpm.conf" ]; then
+        echo '--- Updating /etc/nginx/includes/loc-phpfpm.conf for PHPFPM ---'
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_PHPFPM_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-phpfpm.conf
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_READ_TIMEOUT}!${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_READ_TIMEOUT}!" /etc/nginx/includes/loc-phpfpm.conf
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_PASS}!${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_PASS}!" /etc/nginx/includes/loc-phpfpm.conf
+    fi
 
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_PHPFPM_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-phpfpm.conf
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_READ_TIMEOUT}!${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_READ_TIMEOUT}!" /etc/nginx/includes/loc-phpfpm.conf
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_PASS}!${NGINX_VHOST_UPSTREAM_PHPFPM_FASTCGI_PASS}!" /etc/nginx/includes/loc-phpfpm.conf
+    if [ -f "/etc/nginx/conf.d/upstream.conf" ]; then
+        echo '--- Updating /etc/nginx/conf.d/upstream.conf for PROXYAPP ---'
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_KEEPALIVE}!${NGINX_VHOST_UPSTREAM_PROXYAPP_KEEPALIVE}!" /etc/nginx/conf.d/upstream.conf
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_HOST_PORT}!${NGINX_VHOST_UPSTREAM_PROXYAPP_HOST_PORT}!" /etc/nginx/conf.d/upstream.conf
+    fi
 
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_KEEPALIVE}!${NGINX_VHOST_UPSTREAM_PROXYAPP_KEEPALIVE}!" /etc/nginx/conf.d/upstream.conf
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_PROTOCOL}!${NGINX_VHOST_UPSTREAM_PROXYAPP_PROTOCOL}!" /etc/nginx/includes/loc-proxyapp.conf
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_HOST_PORT}!${NGINX_VHOST_UPSTREAM_PROXYAPP_HOST_PORT}!" /etc/nginx/includes/loc-proxyapp.conf /etc/nginx/conf.d/upstream.conf
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_PASS}!${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_PASS}!" /etc/nginx/includes/loc-proxyapp.conf
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_X_FORWARDED_HOST}!${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_X_FORWARDED_HOST}!" /etc/nginx/includes/loc-proxyapp.conf
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_X_FORWARDED_PROTO}!${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_X_FORWARDED_PROTO}!" /etc/nginx/includes/loc-proxyapp.conf
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_CONNECTION}!${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_CONNECTION}!" /etc/nginx/includes/loc-proxyapp.conf
+    if [ -f "/etc/nginx/includes/loc-proxyapp.conf" ]; then
+        echo '--- Updating /etc/nginx/includes/loc-proxyapp.conf for PROXYAPP ---'
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_PROTOCOL}!${NGINX_VHOST_UPSTREAM_PROXYAPP_PROTOCOL}!" /etc/nginx/includes/loc-proxyapp.conf
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_HOST_PORT}!${NGINX_VHOST_UPSTREAM_PROXYAPP_HOST_PORT}!" /etc/nginx/includes/loc-proxyapp.conf
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_PASS}!${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_PASS}!" /etc/nginx/includes/loc-proxyapp.conf
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_X_FORWARDED_HOST}!${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_X_FORWARDED_HOST}!" /etc/nginx/includes/loc-proxyapp.conf
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_X_FORWARDED_PROTO}!${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_X_FORWARDED_PROTO}!" /etc/nginx/includes/loc-proxyapp.conf
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_CONNECTION}!${NGINX_VHOST_UPSTREAM_PROXYAPP_PROXY_HEADER_CONNECTION}!" /etc/nginx/includes/loc-proxyapp.conf
+    fi
 
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_ECHO_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_ECHO_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-echo.conf
-    sed -i "s!\${NGINX_VHOST_UPSTREAM_MINIO_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_MINIO_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-minio.conf
+    if [ -f "/etc/nginx/includes/loc-echo.conf" ]; then
+        echo '--- Updating /etc/nginx/includes/loc-echo.conf for ECHO ---'
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_ECHO_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_ECHO_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-echo.conf
+    fi
 
-    # Files removal based on condition
+    if [ -f "/etc/nginx/includes/loc-minio.conf" ]; then
+        echo '--- Updating /etc/nginx/includes/loc-minio.conf for MINIO ---'
+        sed -i "s!\${NGINX_VHOST_UPSTREAM_MINIO_SERVICE_HOST_PORT}!${NGINX_VHOST_UPSTREAM_MINIO_SERVICE_HOST_PORT}!" /etc/nginx/includes/loc-minio.conf
+    fi
+
+    ## FILES REMOVAL
+    echo '!!! Nginx files removal based on condition !!!'
+
     if [ "${NGINX_VHOST_ENABLE_HTTP_TRAFFIC}" = "false" ] && [ -f "/etc/nginx/conf.d/default.conf" ]; then
         rm -f /etc/nginx/conf.d/default.conf
     fi
